6fb5bbede814a35dadab6b55b9907ba4e919a8a9
DeeptiKoranga <deeptikoranga1618@gmail.com>
2025-09-29 00:11:54 +0530

    changed .js to .jsx and added .gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-i am solving merge conflict
--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,52 @@
-// client/src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/authContext';
-import { ProgressProvider } from './context/progressContext';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { AuthProvider } from './context/authContext';
+// import { ProgressProvider } from './context/progressContext';
+// import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
-// Page Imports
-import LoginPage from './pages/loginPage';
-import RegisterPage from './pages/registerPage';
-import StudentDashboard from './pages/studentDashboard';
-import FacultyDashboard from './pages/facultyDashboard';
-import PrivateRoute from './components/privateRoute';
+// // Page Imports
+// import LoginPage from './pages/loginPage';
+// import RegisterPage from './pages/registerPage';
+// import StudentDashboard from './pages/studentDashboard';
+// import FacultyDashboard from './pages/facultyDashboard';
+// import PrivateRoute from './components/privateRoute';
 
-const darkTheme = createTheme({ palette: { mode: 'dark' } });
+// const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
-function App() {
-  return (
-    <AuthProvider>
-      <ProgressProvider>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Router>
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
+// function App() {
+//   return (
+//     <AuthProvider>
+//       <ProgressProvider>
+//         <ThemeProvider theme={darkTheme}>
+//           <CssBaseline />
+//           <Router>
+//             <Routes>
+//               {/* Public Routes */}
+//               <Route path="/login" element={<LoginPage />} />
+//               <Route path="/register" element={<RegisterPage />} />
 
-              {/* Protected Student Route */}
-              <Route path="/student-dashboard" element={
-                <PrivateRoute allowedRoles={['student']}>
-                  <StudentDashboard />
-                </PrivateRoute>
-              } />
+//               {/* Protected Student Route */}
+//               <Route path="/student-dashboard" element={
+//                 <PrivateRoute allowedRoles={['student']}>
+//                   <StudentDashboard />
+//                 </PrivateRoute>
+//               } />
 
-              {/* Protected Faculty Route */}
-              <Route path="/faculty-dashboard" element={
-                <PrivateRoute allowedRoles={['faculty']}>
-                  <FacultyDashboard />
-                </PrivateRoute>
-              } />
+//               {/* Protected Faculty Route */}
+//               <Route path="/faculty-dashboard" element={
+//                 <PrivateRoute allowedRoles={['faculty']}>
+//                   <FacultyDashboard />
+//                 </PrivateRoute>
+//               } />
 
-              {/* Default redirect or home page */}
-              <Route path="/" element={<LoginPage />} />
-            </Routes>
-          </Router>
-        </ThemeProvider>
-      </ProgressProvider>
-    </AuthProvider>
-  );
-}
+//               {/* Default redirect or home page */}
+//               <Route path="/" element={<LoginPage />} />
+//             </Routes>
+//           </Router>
+//         </ThemeProvider>
+//       </ProgressProvider>
+//     </AuthProvider>
+//   );
+// }
 
-export default App;
+// export default App;
